45f11101bcc1e6a67de7ed643aa28a91ccac23c6
subpal26 <subpal26@in.ibm.com>
2019-03-22 09:30:17 +0530

    Project Added

---
+++ src/main/java/com/stackroute/kafka/domain/Greetings.java
@@ -0,0 +1,36 @@
+package com.stackroute.kafka.domain;
+
+public class Greetings {
+    private String msg;
+    private String name;
+
+    public Greetings() {
+
+    }
+
+    public Greetings(String msg, String name) {
+        this.msg = msg;
+        this.name = name;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return msg + ", " + name + "!";
+    }
+}
